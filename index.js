8d3d05e164a1c3ab593f3554fd89d305e3836f3a
Raditya <arya.raditya26@gmail.com>
2017-01-17 21:27:28 +0700

    done

--- index.js
+++ index.js
@@ -3,3 +3,56 @@
 import DBModel from "./models/db_model.js";
 import Cohort from "./models/cohort.js";
 import Student from "./models/student.js";
+const repl = require('repl')
+
+var db = new DBModel("./db/student.db");
+
+var argv = process.argv[2]
+  if (argv == 'playtime') {
+    var r = repl.start('> ');
+    r.context.dbModel = db
+    //r.context.student = Student
+    r.context.Student = Student
+    r.context.Cohort = Cohort
+  }
+
+
+// Student.findAll(dbModel.connection, function (err,data){
+//   if (!err) {
+//     for (var i = 0; i < data.length; i++) {
+//       console.log(data[i]);
+//     }
+//   }else {
+//     console.log('Error');
+//   }
+// })
+//
+// Student.where(dbModel.connection, "firstname = 'Radit'", function (err,data){
+//   if (!err) {
+//     for (var i = 0; i < data.length; i++) {
+//       console.log(data[i]);
+//     }
+//   }else {
+//     console.log('error');
+//   }
+// })
+
+// Cohort.findAll(dbModel.connection, function (err,data){
+//   if (!err) {
+//     for (var i = 0; i < data.length; i++) {
+//       console.log(data[i]);
+//     }
+//   }else {
+//     console.log('Error');
+//   }
+// })
+//
+// Cohort.where(dbModel.connection, "name = 'canadianFox'", function (err,data){
+//   if (!err) {
+//     for (var i = 0; i < data.length; i++) {
+//       console.log(data[i]);
+//     }
+//   }else {
+//     console.log('error');
+//   }
+// })
--- models/cohort.js
+++ models/cohort.js
@@ -3,7 +3,87 @@
 import Student from "./student.js";
 
 class Cohort {
+  constructor(name,id) {
+    this.name = name
+    this.id = id
+  }
 
+  static create(connection, input){
+    let db = connection
+    let add = `INSERT INTO cohorts (name) VALUES ('${input.name}');`
+    db.serialize(function() {
+      db.run(add, function (err) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log('new cohort has been created');
+        }
+      })
+    })
+ }
+
+ static update(connection, input){
+   let db = connection
+   let update = `UPDATE cohorts SET name = '${input.name}' WHERE id = '${input.id}';`
+   db.serialize(function() {
+     db.run(update, function (err) {
+       if (err) {
+         console.log(err);
+       }
+       else {
+         console.log('cohort has been updated');
+       }
+     })
+   })
+ }
+
+ static delete(connection, input){
+   let db = connection
+   let remove =`DELETE FROM cohorts WHERE id = '${input}';`;
+   db.serialize(function() {
+     db.run(remove, function (err) {
+       if (err) {
+         console.log(err);
+       }
+       else {
+         console.log('cohort has been removed');
+       }
+     })
+   })
+ }
+
+ static findById(connection, id){
+   let db = connection
+  let find = `SELECT * FROM cohorts WHERE id LIKE '${id}';`;
+   db.serialize(function() {
+     db.each(find, function (err,row) {
+       if (err) {
+         console.log(err);
+       }
+       else {
+         console.log(`cohort found : \n\nID : ${row.id}\nName : ${row.name}\n`);
+       }
+     })
+   })
+ }
+
+ static findAll(connection, cb){
+   let db = connection
+   let find_all = "SELECT * FROM cohorts"
+   db.serialize(function() {
+     db.all(find_all, cb)
+   })
+ }
+
+ static where(connection, val, cb){
+   let db = connection
+   let where = `SELECT * FROM cohorts WHERE `
+   db.serialize(function() {
+     db.all(where + val, cb)
+     console.log(where);
+   })
+ }
 
 }
 
