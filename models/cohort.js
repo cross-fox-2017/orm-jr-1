6abd299b6ad02b5cb56ba29dd7711d504c87adcf
Mangku Widodo <mangkuwi26@gmail.com>
2017-01-17 20:56:57 +0700

    release 0 & 1 done

--- models/cohort.js
+++ models/cohort.js
@@ -3,8 +3,13 @@
 import Student from "./student.js";
 
 class Cohort {
+  constructor(name, id){
+    this.name = name
+    this.id = id
+  }
+  static createCohort(db, obj){
 
-
+  }
 }
 
 export default Cohort
