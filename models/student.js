a3bdd7b920a58ce6977fae04e83d211d594010a4
Syanmil <syanmil@yahoo.com>
2017-01-17 11:13:43 +0700

    initialize connection and creating table

--- models/student.js
+++ models/student.js
@@ -1,8 +1,33 @@
 "use strict"
 
 class Student {
+  create(connection, param){
 
+  }
+  update(connection, param){
 
+  }
+  delete(connection, id){
+
+  }
+  findById(connection, id){
+
+  }
+  findAll(connection, this.callback){
+
+  }
+  where(connection, param, this.callback){
+
+  }
+  callback(data, err){
+    if(!err){
+      for (let i = 0; i < data.length; i++){
+        console.log(data[i]);
+      }
+    } else {
+      console.log('Error');
+    }
+  }
 }
 
 export default Student
