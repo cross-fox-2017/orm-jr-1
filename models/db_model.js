9a066fb39e475c4f0cbdb30aff329a5ded2d3495
Samue PH Sinaga <samuelphsinaga@gmail.com>
2017-01-17 21:15:40 +0700

    release 0, release 1

--- models/db_model.js
+++ models/db_model.js
@@ -1,8 +1,49 @@
 "use strict"
 
+const sqlite = require('sqlite3').verbose();
+
+var CREATE_COHORTS  = "CREATE TABLE IF NOT EXISTS cohorts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);"
+var CREATE_STUDENTS = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, cohort_id INTEGER);"
+
 class DBModel {
+  constructor(file){
+    this.connection = new sqlite.Database(file)
+  }
 
+  setup(){
+    let db = this.connection;
 
+    let createTableStudents = () => {
+      //RUN SQL one at a time
+      db.serialize(function(){
+        //CREATE TABLE
+        db.run(CREATE_STUDENTS, function(err){
+          if(err) {
+            console.log(err);
+          } else {
+            console.log('CREATE STUDENTS succes!');
+          }
+        });
+      });
+    }
+
+    let createTableCohorts = () => {
+      //RUN SQL one at a time
+      db.serialize(function(){
+        //CREATE TABLE
+        db.run(CREATE_COHORTS, function(err){
+          if(err) {
+            console.log(err);
+          } else {
+            console.log('CREATE COHORTS succes!');
+          }
+        });
+      });
+    }
+
+    createTableStudents()
+    createTableCohorts()
+  }
 }
 
 export default DBModel
