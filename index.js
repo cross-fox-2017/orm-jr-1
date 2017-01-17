010745e76eb9e2f2a1b659313a9b6def1782a137
isumizumi <isumi.karina72@gmail.com>
2017-01-17 21:15:16 +0700

    Progress Release 1

--- index.js
+++ index.js
@@ -3,3 +3,17 @@
 import DBModel from "./models/db_model.js";
 import Cohort from "./models/cohort.js";
 import Student from "./models/student.js";
+const repl = require("repl");
+
+
+var db = new DBModel("./db/student.db")
+
+var p = process.argv[2]
+
+if(p == "playtime") {
+	var rpl = repl.start('> ')
+	rpl.context.dbModel = db
+	rpl.context.Student = Student
+}
+
+
--- models/db_model.js
+++ models/db_model.js
@@ -1,7 +1,37 @@
 "use strict"
 
-class DBModel {
+const sqlite = require('sqlite3').verbose()
 
+class DBModel {
+	constructor(file) {
+		this.connection = new sqlite.Database(file)
+	}
+
+	setup() {
+		let db = this.connection
+		let CREATE_TABLE_STUDENT = "CREATE TABLE student (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT NOT NULL, lastname TEXT, cohort_id INTEGER);"
+		let CREATE_TABLE_COHORT = "CREATE TABLE cohort (id INTEGER PRIMARY KEY AUTOINCREMENT, cohort_name TEXT NOT NULL);"
+
+		db.serialize(function(){
+	   		db.run(CREATE_TABLE_STUDENT, function(err) {
+	    		if(err) {
+	        		console.log(err);
+	      		} else {
+	        		console.log('CREATE TABLE STUDENT: SUCCESS!');
+	      		}
+	    	});
+	  	});
+
+	  	db.serialize(function(){
+	   		db.run(CREATE_TABLE_COHORT, function(err) {
+	    		if(err) {
+	        		console.log(err);
+	      		} else {
+	        		console.log('CREATE TABLE COHORT: SUCCESS!');
+	      		}
+	    	});
+	  	});
+	}
 
 }
 
